69229834410b5f00f0cc6392a83b4ec17151a689
lauthrul <lauthrul@foxmail.com>
2020-06-04 20:49:45 +0800

    structure adjustment

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+const (
+	Red    = 5
+	Orange = 4
+	Purple = 3
+	Blue   = 2
+	Green  = 1
+)
+
+type Addition struct {
+	Value  int      `json:"value"`
+	Heroes []string `json:"heroes"`
+}
+
+type Hero struct {
+	Name      string     `json:"name"`
+	Color     int        `json:"color"`
+	Additions []Addition `json:"additions"`
+}
+
+type Config struct {
+	Seats  int             `json:"seats"`
+	InUse  []string        `json:"in_use"`
+	Heroes map[string]Hero `json:"heroes"`
+}
+
+func (this *Config) IsInUse(hero string) bool {
+	for _, name := range this.InUse {
+		if name == hero {
+			return true
+		}
+	}
+	return false
+}
+
+func LoadConfig(file string, config *Config) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		return err
+	}
+	if config.Seats != len(config.InUse) {
+		return fmt.Errorf("invalid config: heroes in use not match seats")
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,66 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strconv"
 	"time"
 
+	"jinrongdiguo/config"
 	"jinrongdiguo/util"
 )
 
-const (
-	Red    = 5
-	Orange = 4
-	Purple = 3
-	Blue   = 2
-	Green  = 1
-)
-
-type Addition struct {
-	Value  int      `json:"value"`
-	Heroes []string `json:"heroes"`
-}
-
-type Hero struct {
-	Name      string     `json:"name"`
-	Color     int        `json:"color"`
-	Additions []Addition `json:"additions"`
-}
-
-type Config struct {
-	Seats  int             `json:"seats"`
-	InUse  []string        `json:"in_use"`
-	Heroes map[string]Hero `json:"heroes"`
-}
-
-func (this *Config) IsInUse(hero string) bool {
-	for _, name := range this.InUse {
-		if name == hero {
-			return true
-		}
-	}
-	return false
-}
-
-func LoadConfig(file string, config *Config) error {
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(data, config)
-	if err != nil {
-		return err
-	}
-	if config.Seats != len(config.InUse) {
-		return fmt.Errorf("invalid config: hero in use not match place")
-	}
-	return nil
-}
-
-func TimeCost() func() {
+func timeCost() func() {
 	start := time.Now()
 	fmt.Println(start)
 	return func() {
@@ -88,27 +38,27 @@ func main() {
 		return
 	}
 
-	defer TimeCost()()
+	defer timeCost()()
 
 	// load config
-	var config Config
-	err = LoadConfig("技能加成.json", &config)
+	var cfg config.Config
+	err = config.LoadConfig("技能加成.json", &cfg)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	// fmt.Println(config)
+	// fmt.Println(cfg)
 
 	// gen all combinations within the given thread numbers
 	var names []string
-	for name, _ := range config.Heroes {
+	for name, _ := range cfg.Heroes {
 		names = append(names, name)
 	}
-	combinations := util.Combination(names, config.Seats, threads)
+	combinations := util.Combination(names, cfg.Seats, threads)
 	// fmt.Println(combinations)
 
 	// choose the highest score heroes from all the combinations
-	var result Addition
+	var result config.Addition
 	for _, group := range combinations {
 		nameMap := map[string]bool{}
 		for _, name := range group {
@@ -117,7 +67,7 @@ func main() {
 
 		value := 0
 		for name, _ := range nameMap {
-			hero := config.Heroes[name]
+			hero := cfg.Heroes[name]
 			for _, addition := range hero.Additions {
 				match := true
 				for _, h := range addition.Heroes {
@@ -141,12 +91,12 @@ func main() {
 	// make suggestion for add new heroes and remove old heroes
 	var add, remove []string
 	for _, name := range result.Heroes {
-		if hero, ok := config.Heroes[name]; ok && !config.IsInUse(name) {
+		if hero, ok := cfg.Heroes[name]; ok && !cfg.IsInUse(name) {
 			add = append(add, hero.Name)
 		}
 	}
-	for name, hero := range config.Heroes {
-		if !config.IsInUse(name) {
+	for name, hero := range cfg.Heroes {
+		if !cfg.IsInUse(name) {
 			continue
 		}
 		match := false
