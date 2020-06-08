d27978a2b8cf6ccbb36c83305a1bca7179d9168d
lauthrul <lauthrul@foxmail.com>
2020-06-08 22:12:32 +0800

    Performance optimize.

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"time"
 
 	"financial_empire/cache"
 	"financial_empire/config"
+	"financial_empire/logger"
 	"financial_empire/util"
 )
 
@@ -16,17 +16,15 @@ const USE_CACHE = false
 
 func timeCost() func() {
 	start := time.Now()
-	fmt.Println(start)
 	return func() {
-		end := time.Now()
 		tc := time.Since(start)
-		fmt.Printf("%v\ntime cost = %v\n", end, tc)
+		logger.Logger.Printf("time cost = %v\n", tc)
 	}
 }
 
 func showResult(result config.Addition, cfg config.Config) {
 	show := config.Addition{
-		Value:result.Value,
+		Value: result.Value,
 	}
 	for _, h := range result.Heroes {
 		for name, hero := range cfg.Heroes {
@@ -35,22 +33,23 @@ func showResult(result config.Addition, cfg config.Config) {
 			}
 		}
 	}
-	fmt.Println(show)
+	logger.Logger.Println(show)
 }
 
 func main() {
 
 	usage := func() {
 		// ./financial_empire 10
-		fmt.Printf("param error. \nusage: %s <thread_nums>\n", os.Args[0])
+		fmt.Printf("usage: %s <config_file> <thread_nums>\n", os.Args[0])
 	}
 
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		usage()
 		return
 	}
 
-	threads, err := strconv.Atoi(os.Args[1])
+	configFile := os.Args[1]
+	threads, err := strconv.Atoi(os.Args[2])
 	if err != nil || threads < 1 {
 		usage()
 		return
@@ -59,15 +58,17 @@ func main() {
 	defer timeCost()()
 
 	// load config
+	logger.Logger.Printf("load config ...")
 	var cfg config.Config
-	err = config.LoadConfig("config.json", &cfg)
+	err = config.LoadConfig(configFile, &cfg)
 	if err != nil {
-		fmt.Printf("load config fail: %s\n", err.Error())
+		logger.Logger.Printf("load config fail: %s\n", err.Error())
 		return
 	}
-	// fmt.Println(cfg)
+	// log.Logger.Println(cfg)
 
 	// gen all combinations within the given thread numbers
+	logger.Logger.Printf("calculate combinations ...")
 	var names []string
 	for name, _ := range cfg.Heroes {
 		names = append(names, name)
@@ -79,31 +80,31 @@ func main() {
 	if USE_CACHE {
 		err = cache.LoadCache(&cch)
 		if err != nil {
-			fmt.Printf("load cache fail: %s\n", err.Error())
+			logger.Logger.Printf("load cache fail: %s\n", err.Error())
 		} else {
 			if cch.Exist(names) {
-				fmt.Printf("read cache\n")
+				logger.Logger.Printf("read cache\n")
 				err = cch.Read(names, &combinations)
 				if err != nil {
-					fmt.Printf("read cache fail: %s\n", err.Error())
+					logger.Logger.Printf("read cache fail: %s\n", err.Error())
 				}
 			}
 		}
 	}
 
-
-	if len(combinations) <= 0{
-		combinations = util.Combination(names, cfg.Seats, threads)
+	if len(combinations) <= 0 {
+		util.Combination(names, uint64(cfg.Seats), uint64(threads), &combinations)
 		if USE_CACHE {
 			err = cch.Save(names, combinations)
 			if err != nil {
-				fmt.Printf("save cache fail: %s\n", err.Error())
+				logger.Logger.Printf("save cache fail: %s\n", err.Error())
 			}
 		}
 	}
-	// fmt.Println(combinations)
+	// log.Logger.Println(combinations)
 
 	// choose the highest score heroes from all the combinations
+	logger.Logger.Printf("choose best solution ...")
 	var result config.Addition
 	for _, group := range combinations {
 		nameMap := map[string]bool{}
@@ -135,6 +136,7 @@ func main() {
 	showResult(result, cfg)
 
 	// make suggestion for add new heroes and remove old heroes
+	logger.Logger.Printf("make suggestion ...")
 	var add, remove []string
 	for _, name := range result.Heroes {
 		if hero, ok := cfg.Heroes[name]; ok && !cfg.IsInUse(name) {
@@ -156,5 +158,5 @@ func main() {
 			remove = append(remove, hero.Name)
 		}
 	}
-	fmt.Printf("suggestion:\n add: %v\n remove: %v\n", add, remove)
+	logger.Logger.Printf("suggestion:\n add: %v\n remove: %v\n", add, remove)
 }
--- util/util.go
+++ util/util.go
@@ -1,54 +1,56 @@
 package util
 
 import (
-	"fmt"
 	"sync"
+
+	"financial_empire/logger"
 )
 
 // 组合（通用数学方法）
 // 参考：https://github.com/mxschmitt/golang-combinations/blob/master/combinations.go
 // 		https://stackoverflow.com/questions/56103775/how-to-print-formatted-string-to-the-same-line-in-stdout-with-go
-func Combination(set []string, size int, threads int) (subsets [][]string) {
+func Combination(set []string, size uint64, threads uint64, subsets *[][]string) {
 	// Go through all possible combinations of objects
 	// from 1 (only first object in subset) to 2^length (all objects in subset)
-	length := uint(len(set))
-	total := 1 << length
+	length := uint64(len(set))
+	total := uint64(1 << length)
 	num := total / threads
 	if total%threads != 0 {
 		threads++
 	}
-	res := make([][][]string, threads)
+	res := make([][]uint64, threads)
 
 	wg := sync.WaitGroup{}
-	wg.Add(threads)
+	wg.Add(int(threads))
 
-	for th := 0; th < threads; th++ {
+	for th := uint64(0); th < threads; th++ {
 		from := th * num
 		if from == 0 {
 			from = 1
 		}
-		end := (th+1)*num
+		end := (th + 1) * num
 		if end > total {
 			end = total
 		}
-		go func(th, from, end int) {
-			fmt.Printf("[#%03d] [%d, %d) (%d)\n", th, from, end, end - from)
+		go func(th, from, end uint64) {
+			logger.Logger.Printf("[#%03d] [%d, %d) (%d)\n", th, from, end, end-from)
 
 			for bit := from; bit < end; bit++ {
-				// fmt.Printf("[%d] %0.2f%%\r", th, float64((bit-from)*100)/float64(end-from))
-				var subset []string
-				for object := uint(0); object < length; object++ {
+				// logger.Logger.Printf("[%d] %0.2f%%\r", th, float64((bit-from)*100)/float64(end-from))
+				count := uint64(0)
+				for object := uint64(0); object < length; object++ {
 					// checks if object is contained in subset
 					// by checking if bit 'object' is set in subsetBits
 					if (bit>>object)&1 == 1 {
-						// add object to subset
-						subset = append(subset, set[object])
+						count++
 					}
 				}
-				// add subset to subsets
-				if len(subset) == size {
-					res[th] = append(res[th], subset)
+				if count != size {
+					continue
 				}
+
+				// add subset to subsets
+				res[th] = append(res[th], bit)
 			}
 			wg.Done()
 		}(th, from, end)
@@ -57,7 +59,15 @@ func Combination(set []string, size int, threads int) (subsets [][]string) {
 	wg.Wait()
 
 	for _, r := range res {
-		subsets = append(subsets, r...)
+		for _, v := range r {
+			// add object to subset
+			var subset []string
+			for object := uint64(0); object < length; object++ {
+				if (v>>object)&1 == 1 {
+					subset = append(subset, set[object])
+				}
+			}
+			*subsets = append(*subsets, subset)
+		}
 	}
-	return subsets
 }
